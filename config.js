bb750f8894433f886df71d44a9bb655e3c843dc7
2puntozeta <2duepunto.zero0@gmail.com>
2026-07-21 15:14:45 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.KDS_CONFIG = {
   SUPABASE_URL: "https://fwehyizmqxinrqzczndz.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_d7IZy6v3mp4pR1rk0ZEvCQ_5S99qjYQ"
+  SUPABASE_ANON_KEY: "sb_publishable_d7IZy6v3mp4pR1rk0ZEvCQ_5S99qjYQ",
+  TABLES: Array.from({ length: 30 }, (_, i) => String(i + 1))
 };
